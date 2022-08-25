91044f0db0f96d39663356759ea343cf793292bd
Pranshu Agrawal <pranshukumar1999@gmail.com>
2022-08-25 11:27:57 +0530

    fix: hover on trash icon

--- frontend/app_flowy/lib/plugins/trash/menu.dart
+++ frontend/app_flowy/lib/plugins/trash/menu.dart
@@ -11,26 +11,43 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:app_flowy/generated/locale_keys.g.dart';
 import 'package:flowy_infra/theme.dart';
+import 'package:flowy_infra_ui/style_widget/hover.dart';
+import 'package:flowy_sdk/protobuf/flowy-folder/view.pb.dart';
 
 class MenuTrash extends StatelessWidget {
-  const MenuTrash({Key? key}) : super(key: key);
+  final bool isSelected;
+  final ViewPB view;
+  final void Function(ViewPB) onSelected;
+
+  MenuTrash({
+    Key? key,
+    required this.view,
+    required this.isSelected,
+    required this.onSelected,
+  }) : super(key: ValueKey('$view.hashCode/$isSelected'));
 
   @override
   Widget build(BuildContext context) {
+    final theme = context.watch<AppTheme>();
     return SizedBox(
-      height: 26,
-      child: InkWell(
-        onTap: () {
-          getIt<MenuSharedState>().latestOpenView = null;
-          getIt<HomeStackManager>()
-              .setPlugin(makePlugin(pluginType: PluginType.trash));
-        },
-        child: _render(context),
-      ),
-    );
+        height: 26,
+        child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4),
+            child: InkWell(
+              onTap: () {
+                getIt<MenuSharedState>().latestOpenView = null;
+                getIt<HomeStackManager>()
+                    .setPlugin(makePlugin(pluginType: PluginType.trash));
+              },
+              child: FlowyHover(
+                style: HoverStyle(hoverColor: theme.bg3),
+                builder: (_, onHover) => _render(context, onHover),
+                setSelected: () => isSelected,
+              ),
+            )));
   }
 
-  Widget _render(BuildContext context) {
+  Widget _render(BuildContext context, bool onHover) {
     return Row(children: [
       ChangeNotifierProvider.value(
         value: Provider.of<AppearanceSettingModel>(context, listen: true),
